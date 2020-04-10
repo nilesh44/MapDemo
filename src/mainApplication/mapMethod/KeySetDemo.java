565c04fd8e5e387aec7812f7b04b99a4c0437243
pooja <pooja@LAPTOP-GR9M4V2E>
2020-04-10 17:26:53 +0530

    update KeySetDemo

--- src/mainApplication/mapMethod/KeySetDemo.java
+++ src/mainApplication/mapMethod/KeySetDemo.java
@@ -21,7 +21,7 @@ public class KeySetDemo {
 		System.out.println(mapOfEmployee);
 
 		System.out.println("\nGIVEN 1 :-map of employee is provided and map is not empty,  ");
-		System.out.println("REQUIREMENT1 :- only wants the set of key for map entries");
+		System.out.println("REQUIREMENT  1 :- only wants the set of key for map entries");
 
 		Set<Integer> keySet = mapOfEmployee.keySet();
 
@@ -29,7 +29,7 @@ public class KeySetDemo {
 		System.out.println(keySet);
 		
 		System.out.println("\nGIVEN 2:-if map is empty  ");
-		System.out.println("REQUIREMENT2 :- only wants the set of key for map entries");
+		System.out.println("REQUIREMENT 2  :- only wants the set of key for map entries");
 		mapOfEmployee  = new HashMap<Integer, Employee>();
 		Set<Integer> keySetforEmptyMap = mapOfEmployee.keySet();
 
@@ -38,7 +38,7 @@ public class KeySetDemo {
 		
 		
 		System.out.println("\nGIVEN 3:-if map is null  ");
-		System.out.println("REQUIREMENT2 :- only wants the set of key for map entries");
+		System.out.println("REQUIREMENT 3 :- only wants the set of key for map entries");
 		mapOfEmployee  = null;
 		try {
 		Set<Integer> keySetforNullMap = mapOfEmployee.keySet();
@@ -54,13 +54,16 @@ public class KeySetDemo {
 		 * {1=[empId=1, empNmae=anil, empSalary=5000.0, empgender=M, empcity=PUNE],
 		 * 4=[empId=5, empNmae=anita, empSalary=7000.0, empgender=F, empcity=MUMBAI]}
 		 * 
-		 * GIVEN 1 :-map of employee is provided and map is not empty, REQUIREMENT1 :-
-		 * only wants the set of key for map entries set of keys:- [1, 4]
+		 * GIVEN 1 :-map of employee is provided and map is not empty,
+		 * REQUIREMENT 1 :- only wants the set of key for map entries
+		 * Result set of keys:- [1, 4]
 		 * 
-		 * GIVEN 2:-if map is empty REQUIREMENT2 :- only wants the set of key for map
-		 * entries set of keys but it is empty:- []
+		 * GIVEN 2:-if map is empty 
+		 * REQUIREMENT 2 :- only wants the set of key for map entries
+		 * Result set of keys but it is empty:- []
 		 * 
-		 * GIVEN 3:-if map is null REQUIREMENT2 :- only wants the set of key for map
+		 * GIVEN 3:-if map is null
+		 * REQUIREMENT :- only wants the set of key for map
 		 * entries java.lang.NullPointerException at
 		 * mainApplication.mapMethod.KeySetDemo.main(KeySetDemo.java:44)
 		 */
